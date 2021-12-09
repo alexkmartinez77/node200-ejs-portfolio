dd1695a87b8a774dc2959e2157846cda91e9bd61
Alex Martinez <alexkmartinez77@gmail.com>
2021-12-09 09:25:32 -0800

    Adds all the project site/github web addresses into respective objects.

--- test/test.js
+++ test/test.js
@@ -25,7 +25,50 @@ describe('Load a Page', function() {
         site: "https://sdcs-web102-astro-calculator.herokuapp.com/",
         gitHub: "https://github.com/alexkmartinez77/web102-astro-weight-calculator"
       },
-      
+      changeCalculator: {
+        site: "https://sdcs-web102-change-calculator.herokuapp.com/",
+        gitHub: "https://github.com/alexkmartinez77/web102-change-calculator"
+      },
+      sanDiegoTopSpots: {
+        site: "https://sdcs-web102-astro-https://sdcs-web102-sd-hot-spots.herokuapp.com/",
+        gitHub: "https://github.com/https://github.com/alexkmartinez77/web102-san-diego-top-spots"
+      },
+      mortgageCalculator: {
+        site: "https://sdcs-web102-https://sdcs-react100-mortgage-calc.herokuapp.com/",
+        gitHub: "https://github.com/alexkmartinez77/react100-mortgage-calculator"
+      },
+      ticTacToe: {
+        site: "https://hidden-chamber-83211.herokuapp.com/",
+        gitHub: "https://github.com/alexkmartinez77/react100-tic-tac-toe"
+      },
+      toDo: {
+        site: "https://sdcs-react100-vstda.herokuapp.com/",
+        gitHub: "https://github.com/alexkmartinez77/react100-vstda"
+      },
+      calorieManager: {
+        site: "https://sdcs-f3.herokuapp.com/",
+        gitHub: "https://github.com/alexkmartinez77/react100-hackathon"
+      },
+      changeCalculator: {
+        site: "https://sdcs-web102-change-calculator.herokuapp.com/",
+        gitHub: "https://github.com/alexkmartinez77/react100-change-calculator"
+      },
+      sanDiegoTopSpots: {
+        site: "https://sdcs-react100-sd-top-spots.herokuapp.com/",
+        gitHub: "https://github.com/alexkmartinez77/react100-san-diego-top-spots"
+      },
+      budgetTracker: {
+        site: "https://sdcs-react200-budget-tracker.herokuapp.com/",
+        gitHub: "https://github.com/alexkmartinez77/react200-budget-tracker"
+      },
+      cityWeather: {
+        site: "https://sdcs-react200-weather-app.herokuapp.com/",
+        gitHub: "https://github.com/alexkmartinez77/react200-weather-app"
+      },
+      movieFinder: {
+        site: "https://sdcs-react200-movie-finder.herokuapp.com/#/",
+        gitHub: "https://github.com/alexkmartinez77/react200-movie-finder"
+      }
     }
   })
 })
